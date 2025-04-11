aa1e13459eb43e6393af2c0fb19a49125a83cb50
yassssml <ycrisostomo051@gmail.com>
2025-04-11 11:14:24 -0300

    Somando até 40

---
+++ while3.js
@@ -0,0 +1,8 @@
+let i = 0;
+let soma = 1;
+
+while (soma <= 30) {
+        soma += i;
+        i++;
+        console.log(`Numero: ${soma + i} , Numero usado: ${i}`);}
+        console.log(`Soma: ${soma}`)       
