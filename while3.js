cfb9f0047dae242c14242d092c88da7e180e753b
yassssml <ycrisostomo051@gmail.com>
2025-04-11 11:23:48 -0300

    Tabuada do 5

--- while3.js
+++ while3.js
@@ -1,7 +1,7 @@
 let i = 0;
 let soma = 1;
 
-while (soma <= 30) {
+while (soma <= 40) {
         soma += i;
         i++;
         console.log(`Numero: ${soma + i} , Numero usado: ${i}`);}
--- while4.js
+++ while4.js
@@ -1,5 +1,7 @@
-let i = 5;
-while (i * i < 10) {
-    console.log(i);
+let i = 1;
+let number = 5;
+while (i <= 10) {
+    console.log(` ${number} * ${i} = ${number * i}`);
     i++;
+
 }
