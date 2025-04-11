f7e9f2a4acc25183a0f6d32a32147d86e78c5d53
yassssml <ycrisostomo051@gmail.com>
2025-04-11 11:39:52 -0300

    Moedas para 12 reais

---
+++ while7.js
@@ -0,0 +1,13 @@
+
+let i = 1;
+let total = 0;
+let total2 = 0;
+
+while (total < 12) {
+    total = total + i;
+    i++;
+    console.log(`total: ${total}`);
+    total2 = total2 + i;
+    i++;
+    console.log(`total2: ${total2}`);
+}
